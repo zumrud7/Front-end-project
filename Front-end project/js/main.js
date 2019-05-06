36dfd8f39500dcdbb8afe3964d18e5a97e7a3457
unknown <zumrudia@code.edu.az>
2019-05-05 22:50:21 -0700

    Front end project completed. All pages are updated

--- Front-end project/js/main.js
+++ Front-end project/js/main.js
@@ -40,17 +40,17 @@ $(document).ready(function () {
 
 
 
-//sticky menu bar and scroll back to top function START
+    //sticky menu bar and scroll back to top function START
     var num = 100
-    $(window).scroll(function(){
-        if ($(window).scrollTop() > num && !$("#back-to-top .top").hasClass("active")){
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > num && !$("#back-to-top .top").hasClass("active")) {
             $('#menu-bar').removeClass("fixed");
-            $("#menu-bar").addClass("fixed"); 
+            $("#menu-bar").addClass("fixed");
             $("#back-to-top .top").show()
 
 
             //scroll back to top function on click of a button START
-            $("#back-to-top .top a").click(function() {
+            $("#back-to-top .top a").click(function () {
                 $("body").scrollTop(0);
             });
             //scroll back to top function on click of a button END
@@ -60,7 +60,7 @@ $(document).ready(function () {
             $("#back-to-top .top").hide()
         }
     });
-//sticky menu bar and scroll back to top function END
+    //sticky menu bar and scroll back to top function END
 
 
 
@@ -152,11 +152,12 @@ $(document).ready(function () {
     })
     //carousel nav button animation show/hide end
 
+
     //Pop Categories section carousel js end
 
 
 
-
+    //GALLERY SECTION FUNCTION START
     //gallery mouseover function for icon display start
     var photo = $("#gallery .photo")
     var titleBtn = $("#gallery .photo .title-btn")
@@ -175,10 +176,52 @@ $(document).ready(function () {
 
     //gallery mouseover function for icon display end
 
-   
+
+    // gallery button active function start
+    var catItem = $("#gallery .category-item")
+    for (let i = 0; i < catItem.length; i++) {
+
+        $(catItem[i]).click(function () {
+            if (!$(catItem[i]).hasClass("active")) {
+                $(catItem).removeClass("active");
+                $(catItem[i]).addClass("active");
+            }
+
+        })
+    }
+    // gallery button active function end
+
+    // gallery filter photos function START
+    $(catItem).click(function () {
+
+
+        var category = $(this).attr("id");
+        // alert(category)
+
+        if (category == "all") {
+
+
+            $("#gallery .photo").addClass("hide");
+            setTimeout(function () {
+
+                $(".photo").removeClass("hide")
+            }, 200)
+        }
+        else {
+            $("#gallery .photo").addClass("hide");
+            setTimeout(function () {
+
+                $("." + category).removeClass("hide")
+            }, 200)
+
+        }
+
+    })
+    // gallery filter photos function END
+    //GALLERY SECTION FUNCTION END
+
 
     // our teachers hover js start
-
     var cardImg = $("#our-teachers .card-img-top")
     var logos = $("#our-teachers .card-img-top .social-media")
     for (let i = 0; i < cardImg.length; i++) {
@@ -193,8 +236,9 @@ $(document).ready(function () {
         })
 
     }
-
     // our teachers hover js end
+
+    //STUDENT SAY SECTION CAROUSEL START
     $("#student-say .owl-carousel").owlCarousel({
         loop: true,
         margin: 10,
@@ -216,12 +260,12 @@ $(document).ready(function () {
         }
 
     })
+    //STUDENT SAY SECTION CAROUSEL END
 
-
-    // our block icon animation on hover start
+    // our-blog icon animation on hover start
     var myImg = $("#our-blog .card")
     var myIcon = $("#our-blog .card-img-top .icon")
-    for (let i = 0; i < cardImg.length; i++) {
+    for (let i = 0; i < myImg.length; i++) {
         $(myImg[i]).mouseover(function () {
             $(myIcon[i]).stop();
             $(myIcon[i]).animate({ top: "75px" });
@@ -255,96 +299,175 @@ $(document).ready(function () {
             }
         }
     })
-// our client icon animation end
+    // our client icon animation end
 
 
-// gallery button active function start
-var catItem = $("#gallery .category-item")
-for (let i = 0; i < catItem.length; i++) {
 
-    $(catItem[i]).click(function () {
-        if (!$(catItem[i]).hasClass("active")) {
-            $(catItem).removeClass("active");
-            $(catItem[i]).addClass("active");
+
+
+    // our-courses page pagination change function start
+    var myPages = $("#our-courses .page li")
+
+    for (let i = 0; i < myPages.length; i++) {
+        $(myPages[i]).click(function (ev) {
+
+
+            if ($(myPages[i]).hasClass("disabled")) {
+                $(this).removeClass("active")
+            }
+            else {
+
+                $(myPages).removeClass("active")
+                $(myPages[i]).addClass("active")
+            }
+
+        })
+    }
+    // our-courses page pagination change function end  
+
+
+    // events pagination change function start
+    var myPagination = $("#events .page li")
+
+    for (let i = 0; i < myPagination.length; i++) {
+        $(myPagination[i]).click(function (ev) {
+
+
+            if ($(myPagination[i]).hasClass("disabled")) {
+                $(this).removeClass("active")
+            }
+            else {
+
+                $(myPagination).removeClass("active")
+                $(myPagination[i]).addClass("active")
+            }
+
+        })
+    }
+
+    // events pagination change function end
+
+
+
+    // event-detail form submit comment function start
+    var submitBtn = $("#event-detail form button");
+
+    $(submitBtn).click(function () {
+        if ($("#event-detail #validationName").val() == "") {
+            $("#event-detail form .alert").show()
         }
-        
+
+        else if ($("#event-detail #validationEmail").val() == "" || $("#event-detail textarea").val() == "") {
+            $("#event-detail form .alert").text("Please fill Email, Phone, Subject fields!");
+            $("#event-detail form .alert").show();
+        }
+
     })
-}
-// gallery button active function end
 
-// gallery filter photos function START
-$(catItem).click(function(){
+    // input background change 
+    $("#event-detail .comment input").on("input", function () {
 
-   
-    var category = $(this).attr("id");
-    // alert(category)
+        $("#event-detail .comment input").blur(function () {
+            $(this).css({
+                backgroundColor: "#d8ebff",
+                color: "#034a96"
+            })
+        })
+    })
+    // input background change 
 
-    if(category == "all"){
+    // event-detail form submit comment function end
 
-        
-         $("#gallery .photo").addClass("hide");
-         setTimeout(function(){
 
-            $(".photo").removeClass("hide")
-         }, 200)
+    // join now countdown date START
+
+    var deadline = new Date("Aug 7, 2019 17:17:17").getTime();
+
+    setInterval(function () {
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = deadline - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+        $("#event-detail .join-now .days").text(days);
+        $("#event-detail .join-now .hours").text(hours);
+        $("#event-detail .join-now .minutes").text(minutes);
+        $("#event-detail .join-now .seconds").text(seconds);
+
+
+    }, 1000);
+
+    // join now countdown date END
+
+// course-detail page curriculum tab accordion menu START
+    var curriculumTitle = $("#course-detail .curriculum-tab li");
+    var curriculumIcon = $("#course-detail .curriculum-tab li .fa-angle-down")
+    var curriculumBody = $("#course-detail .curriculum-tab p");
+
+    for (let i = 0; i < curriculumTitle.length; i++) {
+        $(curriculumTitle[i]).click(function () {
+
+            $(curriculumBody[i]).slideToggle();
+            $(curriculumIcon[i]).toggleClass("fa-angle-up");
+
+        })
+
     }
-    else {
-        $("#gallery .photo").addClass("hide");
-        setTimeout(function(){
 
-            $("." + category).removeClass("hide")
-        }, 200)
-        
-    }
+// course-detail page curriculum tab accordion menu END
 
-    
+// course detail review-tab "add a review section" increase star function start
 
-    
+$("#course-detail #s1").click(function(){
+    $("#course-detail #reviews form .fa-star").css("color", "#888888")
+    $("#course-detail #s1").css("color", "#FFBC09");
 })
-// gallery filter photos function END
+$("#course-detail #s2").click(function(){
+    $("#course-detail #reviews form .fa-star").css("color", "#888888")
+    $("#course-detail #s1, #s2").css("color", "#FFBC09");
+})
+$("#course-detail #s3").click(function(){
+    $("#course-detail #reviews form .fa-star").css("color", "#888888")
+    $("#course-detail #s1, #s2, #s3").css("color", "#FFBC09");
+})
+$("#course-detail #s4").click(function(){
+    $("#course-detail #reviews form .fa-star").css("color", "#888888")
+    $("#course-detail #s1, #s2, #s3, #s4").css("color", "#FFBC09");
+})
+$("#course-detail #s5").click(function(){
+    $("#course-detail #reviews form .fa-star").css("color", "#888888")
+    $("#course-detail #s1, #s2, #s3, #s4, #s5").css("color", "#FFBC09");
+})
+// course detail review-tab "add a review section" increase star function end
 
 
 
-// our-courses page pagination change function start
-var myPages = $("#our-courses .page li")
+// contact page form submit function START
+var formBtn = $("#contact form button");
 
-for (let i = 0; i < myPages.length; i++) {
-    $(myPages[i]).click(function(ev){
+$(formBtn).click(function () {
+    if ($("#contact .form-location #validationName").val() == "") {
+        $("#contact .form-location form .alert").show()
+    }
 
-        
-        if($(myPages[i]).hasClass("disabled")){
-            $(this).removeClass("active")
-        }
-        else {
+    else if ($("#event-detail #validationEmail, #validationPhone, #validationSubject, textarea").val() == "") {
+        $("#contact form .alert").text("Please fill Email, Phone, Subject fields!");
+        $("#contact form .alert").show();
+    }
 
-            $(myPages).removeClass("active")
-            $(myPages[i]).addClass("active")
-        }
-
-    })
-}
-// our-courses page pagination change function end  
+})
+// contact page form submit function END
 
 
-// events pagination change function start
-var myPagination = $("#events .page li")
 
-for (let i = 0; i < myPagination.length; i++) {
-    $(myPagination[i]).click(function(ev){
 
-        
-        if($(myPagination[i]).hasClass("disabled")){
-            $(this).removeClass("active")
-        }
-        else {
 
-            $(myPagination).removeClass("active")
-            $(myPagination[i]).addClass("active")
-        }
-
-    })
-}
-
-// events pagination change function end
 
 })
